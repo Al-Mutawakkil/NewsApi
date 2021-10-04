5aa0eeba7a0a53be3e94c950383681f3c2e5c1b8
Al-Mutawakkil <thufails505@gmail.com>
2021-10-04 08:31:06 +0700

    Initial Commit

---
+++ Shared/NewsApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApiApp.swift
+//  Shared
+//
+//  Created by Numair on 04/10/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
